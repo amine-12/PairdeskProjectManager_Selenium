90860fa99fd72ad2696870770aa31c715987b979
amine-12 <54552812+amine-12@users.noreply.github.com>
2021-12-16 02:54:24 -0500

    first commit

---
+++ src/test/java/com/example/PairdeskProjectManager_Selenium/FeatureListPageTest.java
@@ -0,0 +1,40 @@
+package com.example.PairdeskProjectManager_Selenium;
+
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.concurrent.TimeUnit;
+
+@ExtendWith(SeleniumExtension.class)
+public class FeatureListPageTest {
+    ChromeDriver driver;
+
+    public FeatureListPageTest(ChromeDriver driver) {
+        this.driver = driver;
+    }
+
+    @Test
+    void FeatureListPage_Navigation(){
+        try {
+            driver.get("http://localhost:8081");
+            driver.manage().window().maximize();
+            TimeUnit.SECONDS.sleep(2);
+            WebElement ele = driver.findElement(By.xpath("//*[@id=\"nav\"]/nav/ul/li[3]/a/a"));
+            Actions action = new Actions(driver);
+            action.moveToElement(ele).perform();
+            TimeUnit.SECONDS.sleep(2);
+            driver.findElement((By.linkText("FEATURES"))).click();
+            TimeUnit.SECONDS.sleep(2);
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
